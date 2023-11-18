e3858a75fa5a1d01b793e48bb00976e20eb77437
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-18 15:13:36 -0500

    added another solution

--- src/index.js
+++ src/index.js
@@ -1,50 +1,85 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const formElement = document.getElementById('create-task-form')
+
+  formElement.addEventListener('submit', (e) => {e.preventDefault()
+
+
+  const newTaskdescription = document.getElementById('new-task-description')
+  
+  
+  const li = document.createElement('li')
+  li.textContent = newTaskdescription.value
+  
+  const ulElement = document.getElementById('tasks')
+    ulElement.appendChild(li)
+
+    const xButton = document.createElement('button')
+    xButton.textContent = ' X '
+    li.appendChild(xButton)
+    formElement.reset()
+
+xButton.addEventListener('click', (e) => {
+  e.target
+  console.log(e.target.parentNode.remove())
+})
+
+
+  })
+})
+
+
+
+
+
+
+
+
 
   //Selecting form element to prevent default reloading when form submitted
-  const form = document.getElementById('create-task-form') //
-  form.addEventListener('submit', e => e.preventDefault())
+//   const form = document.getElementById('create-task-form') //
+//   form.addEventListener('submit', e => e.preventDefault())
 
-//selecting elements 
-  const submitButton = document.querySelectorAll('form input')[1]
-  const taskDescriptionBox = document.getElementById('new-task-description')
-  const xButton = document.createElement('button')
+// //selecting elements 
+//   const submitButton = document.querySelectorAll('form input')[1]
+//   const taskDescriptionBox = document.getElementById('new-task-description')
+//   const xButton = document.createElement('button')
 
-  //adding event listener to submit button
-  submitButton.addEventListener('click', (e) => {
-    const ul = document.getElementById('tasks')
-    const newLi = document.createElement('li')
-    newLi.textContent = taskDescriptionBox.value
+//   //adding event listener to submit button
+//   submitButton.addEventListener('click', (e) => {
+//     const ul = document.getElementById('tasks')
+//     const newLi = document.createElement('li')
+//     newLi.textContent = taskDescriptionBox.value
     
-    // Created x button to delete task and appending it to li tag
-    xButton.textContent = ' X '
+//     // Created x button to delete task and appending it to li tag
+//     xButton.textContent = ' X '
 
-    newLi.appendChild(xButton)
-    ul.appendChild(newLi)
-    form.reset()  ////resetting entire form page once submit button is clicked
-})
+//     newLi.appendChild(xButton)
+//     ul.appendChild(newLi)
+//     form.reset()  ////resetting entire form page once submit button is clicked
+// })
 
-// event listener to remove task or button's parent note(li tag) when x is 'clicked'
-xButton.addEventListener('click', (e) => {
-  e.target.parentNode.remove()
-})
-})
+// // event listener to remove task or button's parent note(li tag) when x is 'clicked'
+// xButton.addEventListener('click', (e) => {
+//   e.target.parentNode.remove()
+// })
+// })
 
 
 
-//dropdown menu
-const dropDown = document.createElement('select')
-form.appendChild(dropDown)
+// //dropdown menu
+// const dropDown = document.createElement('select')
+// form.appendChild(dropDown)
 
-let options = ['Red', 'Yellow', "Green"]
-for(let i = 0; let i < 3; i++) {
-  let option = options[i]
+// let options = ['Red', 'Yellow', "Green"]
+// for(let i = 0; let i < 3; i++) {
+//   let option = options[i]
 
-  const dropDownOption = document.createElement('option')
-dropDownOption.textContent = options[i]
-dropDown.appendChild(dropDownOption)
-​​
-}
+//   const dropDownOption = document.createElement('option')
+// dropDownOption.textContent = options[i]
+// dropDown.appendChild(dropDownOption)
+// ​​
+// }
 
 
 
